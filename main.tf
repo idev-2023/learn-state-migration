a919531d12c2422b2738dfac26db524739d323ca
idev-2023 <cyvyard2019@gmail.com>
2023-06-15 16:09:51 -0700

    sync

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@
 ## Terraform configuration
 
 terraform {
+  cloud {
+    organization = "terraform-cloud-org1"
+    workspaces {
+      name = "learn-terraform-cloud-migrate"
+    }
+  }
+  
   required_providers {
     random = {
       source  = "hashicorp/random"
